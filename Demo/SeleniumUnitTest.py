06e3404cbbdacdde5f553e38bc8e0cda6aa24aa5
Ratnakara_rao <ltrao2015@gmail.com>
2024-03-12 22:47:59 +0530

    Selenium Unit Test

---
+++ Demo/SeleniumUnitTest.py
@@ -0,0 +1,60 @@
+import time
+import unittest
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+class MyTestCase(unittest.TestCase):
+    """This setup class going to run all the requested tests in only one instance of browser"""
+
+    @classmethod
+    def setUpClass(Cls):
+        Cls.driver = webdriver.Chrome()
+        Cls.driver.implicitly_wait(10)
+        Cls.driver.maximize_window()
+
+    """This will run test browser twice"""
+
+    # def setUp(self):
+    #     self.driver = webdriver.Chrome()
+    #     self.driver.implicitly_wait(10)
+    #     self.driver.maximize_window()
+
+    def test_search_1(self):
+        self.driver.get("https://google.com")
+        time.sleep(3)
+        self.driver.find_element(By.NAME, "q").send_keys("Automation step by step")
+        time.sleep(3)
+        act = ActionChains(self.driver)
+        (act.send_keys_to_element(self.driver.find_element(By.XPATH, "//textarea[@id='APjFqb']"))
+         .send_keys(Keys.ENTER).perform())
+        x = self.driver.title
+        print(x)
+        self.assertEqual(x, "Automation step by step - Google Search")
+
+    def test_search_2(self):
+        self.driver.get("https://google.com")
+        time.sleep(3)
+        self.driver.find_element(By.NAME, "q").send_keys("Tagore ratnakar")
+        time.sleep(3)
+        act = ActionChains(self.driver)
+        (act.send_keys_to_element(self.driver.find_element(By.XPATH, "//textarea[@id='APjFqb']"))
+         .send_keys(Keys.ENTER).perform())
+        x = self.driver.title
+        print(x)
+        self.assertEqual(x, "Tagore ratnakar - Google Search")
+
+    @classmethod
+    def tearDownClass(Cls):
+        Cls.driver.close()
+        Cls.driver.quit()
+
+    # def tearDown(self):
+    #     self.driver.close()
+    #     self.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
